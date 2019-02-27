4e20dc7ff9549c7b1cb11bd156f33087260c040c
kumi <>
2019-02-28 00:50:11 +0900

    fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,7 +16,7 @@ class TasksController < ApplicationController
     task = Task.new(task_params)
     task.save!
     redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
-
+  end
   def task_params
     params.require(:task).permit(:name, :description)
   end
